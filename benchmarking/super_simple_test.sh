967ea928bcacc126f2e5e62b14125363c60bc7ce
Michael Neale <michael.neale@gmail.com>
2025-03-08 16:16:39 +1100

    nearly working

--- benchmarking/super_simple_test.sh
+++ benchmarking/super_simple_test.sh
@@ -2,10 +2,19 @@
 
 # Super Simple Gosling App Test Script
 
+# Default message if none provided
+MESSAGE=${1:-"Add contact named James Gosling"}
+
+# Function to escape spaces in a string
+escape_spaces() {
+    # Replace each space with a backslash followed by a space
+    echo "${1// /\\ }"
+}
+
 # Get UI hierarchy
-echo "Dumping UI hierarchy..."
-adb shell uiautomator dump
-adb pull /sdcard/window_dump.xml
+#echo "Dumping UI hierarchy..."
+#adb shell uiautomator dump
+#adb pull /sdcard/window_dump.xml
 
 # Just use hardcoded coordinates based on the XML we already analyzed
 # Input field center (from previous analysis)
@@ -23,9 +32,10 @@ echo "Clicking input field..."
 adb shell input tap $INPUT_X $INPUT_Y
 sleep 1
 
-# Type text
-echo "Typing text..."
-adb shell input text "This\ is\ a\ simple\ test\ message"
+# Type text - escape spaces in the message
+ESCAPED_MESSAGE=$(escape_spaces "$MESSAGE")
+echo "Typing text: $MESSAGE"
+adb shell input text "$ESCAPED_MESSAGE"
 sleep 1
 
 # Click submit
